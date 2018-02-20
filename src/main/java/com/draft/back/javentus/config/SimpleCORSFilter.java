a6c20e3338fa9050e4b71b32dfa21dbd31bcc64b
Lucas Norberto <lucasnorbertooliveira@gmail.com>
2018-02-20 07:33:17 -0300

    SimplesCorsFilter

---
+++ src/main/java/com/draft/back/javentus/config/SimpleCORSFilter.java
@@ -0,0 +1,40 @@
+package com.draft.back.javentus.config;
+
+import java.io.IOException;
+import java.util.logging.Filter;
+import java.util.logging.LogRecord;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author lucas
+ */
+@Component
+public class SimpleCORSFilter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Expose-Headers", "Location");
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterConfig filterConfig) {}
+
+    public void destroy() {}
+
+    @Override
+    public boolean isLoggable(LogRecord record) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
